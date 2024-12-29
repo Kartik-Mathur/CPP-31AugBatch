996cef9adc537da7e727a4b255df6c98c12ba6a8
Kartik <kartik.mathur10@gmail.com>
2024-12-29 15:15:38 +0530

    Stack and Queue

--- L24-Queue/LargestRectangle.cpp
+++ L24-Queue/LargestRectangle.cpp
@@ -19,24 +19,44 @@ public:
 		stack<int> s;
 		int n = h.size();
 		ns[n - 1] = n;
-		for (int i = n - 2; i >= 0; ++i)
+		s.push(n - 1);
+		for (int i = n - 2; i >= 0; --i)
 		{
+			while (!s.empty() and h[i] <= h[s.top()]) s.pop();
 
+			if (s.empty()) ns[i] = n;
+			else ns[i] = s.top();
 
 			s.push(i);
 		}
 	}
+
 	int largestRectangleArea(vector<int>& h) {
 		int n = h.size();
-
 		vector<int> ps(n), ns(n);
 		previousSmaller(ps, h);
-
+		nextSmaller(ns, h);
+		int ans = 0;
 		for (int i = 0; i < n; ++i)
 		{
-			cout << ps[i] << " ";
+			int len = ns[i] - ps[i] - 1;
+			int area = len * h[i];
+
+			ans = max(ans, area);
 		}
 
-		return -1;
+		return ans;
 	}
 };
+
+
+
+
+
+
+
+
+
+
+
+
